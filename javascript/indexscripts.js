e86e66093665c4a9b7177ea97243a1dfcb90d580
Josh <108278844+JoshH265@users.noreply.github.com>
2023-06-19 18:29:35 +0100

    Making changes

---
+++ javascript/indexscripts.js
@@ -0,0 +1,15 @@
+function updateProgressBarMaths(progress) {
+    var progressBar = document.getElementById('my-progress-bar-maths');
+    progressBar.style.width = progress + '%';
+  }
+
+  
+
+function updateProgressBarCode(progress) {
+    var progressBar = document.getElementById('my-progress-bar-code');
+    progressBar.style.width = progress + '%';
+  }
+
+  updateProgressBarMaths(34);
+  updateProgressBarCode(82);
+
