156af28c71d772aab9fbee4f0f8e7ce6cb035bc8
Josh <108278844+JoshH265@users.noreply.github.com>
2023-06-21 22:11:46 +0100

    Working on programming question section

--- javascript/codingscript.js
+++ javascript/codingscript.js
@@ -1,11 +1,4 @@
 document.getElementById('run-code').addEventListener('click', function() {
-    let userCode = document.getElementById('user-code').value;
-    let outputArea = document.getElementById('output-area');
-
-    try {
-        let output = eval(userCode);
-        outputArea.innerText = `Result: \n${output}`;
-    } catch (error) {
-        outputArea.innerText = `Error: \n${error}`;
-    }
+    var userCode = document.getElementById('user-code').value;
+    document.getElementById('output-area').innerText = userCode;
 });
