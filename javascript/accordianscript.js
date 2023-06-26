ca040176835aa586d070cf4838d56bbf6c2b89f4
CloverDragana <blaacklist@live.com>
2023-06-26 14:22:26 +0100

    added accordians  for links to maths  questions

---
+++ javascript/accordianscript.js
@@ -0,0 +1,15 @@
+var groupAcc = document.getElementsByClassName("topicAccordian");
+var i;
+
+for (i = 0;i < groupAcc.length;i++) {
+    groupAcc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display == "block"){
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+    });
+}
